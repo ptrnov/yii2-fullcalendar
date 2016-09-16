baf8b61735a2987482c1549a3d87d3b79f9d6f7a
ptrnov <piter@lipat.co.id>
2016-09-16 20:55:45 +0700

    piter

---
+++ MomentAsset.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ptrnov\fullcalendar;
+
+/**
+ * Class MomentAsset
+ * @package edofre\fullcalendarscheduler
+ */
+class MomentAsset extends \yii\web\AssetBundle
+{
+	/** @var  array  The javascript file for the Moment library */
+	public $js = [
+		'moment.js',
+	];
+	/** @var  string  The location of the Moment.js library */
+	public $sourcePath = '@bower/moment';
+}
---
+++ ThemeAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ptrnov\fullcalendar;
+
+/**
+ * Class ThemeAsset
+ * @package edofre\fullcalendarscheduler
+ */
+class ThemeAsset extends \yii\web\AssetBundle
+{
+	/** @var array The dependencies for the ThemeAsset bundle */
+	public $depends = [
+		'yii\jui\JuiAsset',
+	];
+}
