2bab498c60593a7ffc0398be8bffeeedf3767e84
ptrnov <piter@lipat.co.id>
2016-09-19 04:30:26 +0700

    piter

--- test/FullcalendarScheduler-firsttime.php
+++ test/FullcalendarScheduler-firsttime.php
@@ -27,11 +27,6 @@ class FullcalendarScheduler extends \yii\base\Widget
 	public $events = [];
 	
 	/**
-	 * @var string url controller Drop url.
-	 */	
-	public $eventDropUrl = NULL;
-	
-	/**
      * Will hold an url to json formatted events!
      * @var url to json service
      */
@@ -203,7 +198,7 @@ class FullcalendarScheduler extends \yii\base\Widget
         if ($this->eventAfterAllRender){
             $options['eventAfterAllRender'] = new JsExpression($this->eventAfterAllRender);
         }
-		$options['events'] =
+		
 		// Load the events
 		$options['events'] = $this->events;
 		$options['resources'] = $this->resources;
